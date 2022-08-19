849425e11840e989e1422de249ea041eb2da08a4
shakeelman <90919562+shakeelman@users.noreply.github.com>
2022-08-19 18:39:10 +0530

    GITHUB
    LGM task 3

---
+++ task-03-main/style.css
@@ -0,0 +1,168 @@
+*{
+    margin: 0;
+    padding: 0;
+    color:rgb(255, 255, 255);
+  }
+  
+  body {
+  height: 89vh;
+  align-items: center;
+  background-color: white;
+  
+  
+  }
+  
+  .nav{
+   /* background-color: rgb(4, 6, 161);  */
+    color: white;
+    position: fixed;  
+    left: 0;   
+    right: 0;  
+    top: 5px;  
+    height: 40px;  
+    font-size: 24px;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    display: flex;  
+    align-items: center; 
+    justify-content: center; 
+    box-shadow: 0 0 25px 0 rgb(243, 243, 243);  
+  }
+  #first1,#email1,#formFile,#no1 {
+  border: 2px solid blueviolet;
+    border-radius: 3px;
+   
+}
+#li {
+  margin-left: 60%;
+  cursor: pointer;
+  flex-direction: column;
+}
+#li1 {
+  margin-right: 0%;
+  cursor: pointer;
+  flex-direction: column;
+}
+  .container{
+    margin-top: 5%;
+    margin-left: 15%;
+    margin-right: 15%;
+    margin-bottom: 2%;
+  
+  }
+
+#img {
+  width: 100px;;
+}
+
+  .app__navbar-links {
+    flex: 1;
+    display: flex;
+    justify-content: right;
+    align-items: center;
+    list-style: none;
+  }
+  app__navbar-logo {
+    display: flex;
+    justify-content: flex-start;
+    align-items: left;
+
+   
+}
+.main {
+  font-family: 'Ubuntu', sans-serif;
+  color: white;
+  height: 70px;
+  padding: 20px;
+  padding-top: 20px;
+}
+  #at {
+    color: #0d0d0d;
+    text-decoration: none;
+    flex-direction: column;
+    text-transform: uppercase;
+    font-weight: 800;
+    transition: all 0.3s ease-in-out;
+    -webkit-transition: all 0.3s ease-in-out;
+    -moz-transition: all 0.3s ease-in-out;
+    -ms-transition: all 0.3s ease-in-out;
+    -o-transition: all 0.3s ease-in-out;
+
+}
+  .row{
+    margin-left: auto;
+    margin-right: auto;
+  }
+  @media screen and (max-width: 600px) {
+    .column {
+      width: 100%;
+      display: block;
+      margin-bottom: 20px;
+    }
+  }
+  
+ 
+  div.sec-form, div.sec-user{
+    justify-content: center ;
+    background: rgb(0 0 0 / 46%);
+    padding-left: 5%;
+    padding-right: 5%;
+  }
+  
+  div.sec-form h1, div.sec-user h1{
+    padding-top: 3%;
+    text-align: center;
+  }
+  
+  div .gender{
+    margin-right: 2%;
+  }
+  
+  div .check2{
+    display: flex;
+  }
+  #img {
+    width: 100px;;
+}
+  
+  div .check1{
+    display: flex;
+  }
+  15
+  .btn2{
+    display: flex;
+    text-align: center;
+    justify-content: space-between;
+    font-weight: bolder;
+  }
+
+
+  .card-body p, .card-body h5{
+    color: #000 ;
+    border-radius: 40px;
+    
+  }
+
+    .card1 {
+      margin-top: 0px;
+     align-content: center;
+     width: 300px;
+     margin: 0 auto;
+     border-radius: 40px; 
+     
+  }
+ #one{
+   color: white;
+ }
+
+  
+  button.btn.btn-primary.col-12.col-sm-9.enroll1 {
+    color: #fff;
+    background-color: #0c3066;
+    border-color: #0d6efd;
+    width: 70px;
+}
+button.btn.btn-danger.col-8.col-sm-2.clear1 {
+  color: #fff;
+  background-color: #233d86;
+  border-color: #f4d2d6;
+}
